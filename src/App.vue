3976f28b94ff4e0c527c20f1e30b4a8921a875a1
Jesus Aguero <roxanap30>
2022-08-09 11:29:57 -0400

    Primera practica Vue

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="count"/>
+ 
+    <myButton :name="name" :contador="count"/>
+    <button @click="incrementCont" > {{nombre}} </button>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+import myButton from './components/myButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    myButton
+},
+data: ()=>({
+
+  count:10,
+  name:"algo",
+  nombre:"incrementar"
+  
+
+}
+),
+methods:{
+
+  incrementCont(){
+    this.count++
+}
+
+}
+
+}
+</script>
+
+<style>
+
+  #app{
+    text-align: center;
+
+
+
+
+  }
+
+</style>
---
+++ src/components/myButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="botom">
+
+    <button>{{name}} {{contador}}</button>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'newButton',
+  props: {
+    name: String,
+    contador:Number
+  }
+
+}
+</script>
+
